c65c4ef06b845edc9808b5857791ab13fc4c5c29
PatrickMihalcea <patrick.mihalcea@gmail.com>
2025-02-06 22:56:44 -0500

    Angular Build

---
+++ src/app/three-js-scene/three-js-scene.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-js-scene',
+  imports: [],
+  templateUrl: './three-js-scene.component.html',
+  styleUrl: './three-js-scene.component.css'
+})
+export class ThreeJsSceneComponent {
+
+}
