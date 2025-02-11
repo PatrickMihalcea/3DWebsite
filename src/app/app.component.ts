6f4ef00a4104316b28ee26da2026b4fd72daad20
Patrick Mihalcea <patrick.mihalcea@capco.com>
2025-02-11 15:28:11 -0500

    Implemented angular-three

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { Scene } from './3DComponents/scene.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgtCanvas, extend } from 'angular-three';
+
+import * as THREE from 'three';
+extend(THREE);
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, NgtCanvas],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
+  protected scene = Scene;
   title = '3dwebsite';
 }
